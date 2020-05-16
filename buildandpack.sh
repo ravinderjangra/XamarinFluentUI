ee05fc5611ef91abaddd5ede1413a86c011f34fa
Ravinder Jangra <ravinderjangra@live.com>
2020-05-17 00:49:11 +0530

    add visual project and generate package on build

--- buildandpack.sh
+++ buildandpack.sh
@@ -7,8 +7,14 @@ MSBuild FluentUI.Android/FluentUI.Android.csproj /t:Clean /p:Configuration=Relea
 # clean ios build
 MSBuild FluentUI.iOS/FluentUI.iOS.csproj /t:Clean /p:Configuration=Release
 
+# clean visual build
+MSBuild FluentUI.Visual/FluentUI.Visual.csproj /t:Clean /p:Configuration=Release
+
 # build android project
-MSBuild FluentUI.Android/FluentUI.Android.csproj  /p:Configuration=Release
+MSBuild FluentUI.Android/FluentUI.Android.csproj  /p:Configuration=Release -t:pack
 
 # build ios project
-MSBuild FluentUI.iOS/FluentUI.iOS.csproj  /p:Configuration=Release
+MSBuild FluentUI.iOS/FluentUI.iOS.csproj  /p:Configuration=Release -t:pack
+
+# build Xamarin.Forms.Visual
+MSBuild FluentUI.Visual/FluentUI.Visual.csproj  /p:Configuration=Release -t:pack
